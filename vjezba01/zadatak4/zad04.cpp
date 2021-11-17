5052d0f77d3866c4655fd1c258750203b8ce489b
Petar1107 <92747146+Petar1107@users.noreply.github.com>
2021-11-17 03:37:57 +0100

    Create zad04.cpp

---
+++ vjezba01/zadatak4/zad04.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int rek_min_num(int* arr, int e)
+{
+    if(e==1)
+        return arr[0];
+    else
+        return min(arr[e-1],rek_min_num(arr,e-1));
+}
+int rek_max_num(int* arr, int e)
+{
+    if (e==1)
+        return arr[0];
+    return max(arr[e-1], rek_max_num(arr,e-1));
+}
+void input_arr(int* arr, int e)
+{
+    int count;
+    for(count=0;count<e;count++)
+    {
+        cin >> (arr[count]);
+    }
+}
+int main()
+{
+    int arr[100];
+    int elements;
+    cout << "unesite broj clanova niza: " << endl;
+    cin >> elements;
+    cout << "unesite elemente niza: " << endl;
+    input_arr(arr,elements);
+    cout << "najveci element u nizu je: " << rek_max_num(arr,elements) << endl;
+    cout << "najmanji element u nizu je: " << rek_min_num(arr,elements) << endl;
+
+}
